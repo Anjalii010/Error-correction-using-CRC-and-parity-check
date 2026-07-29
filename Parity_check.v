18da57e961b4cc6d0057b9cf52e7912359f78cb3
AnjaliChaubey <anjalichaubeyanjali@gmail.com>
2026-07-30 00:27:49 +0530

    Add ParityChecker module to compute parity

---
+++ Parity_check.v
@@ -0,0 +1,9 @@
+// Parity Checker module: Computes the parity of the input data
+module ParityChecker (
+    input wire [14:0] data, // Data input
+    output reg parity       // Parity output
+);
+    always @* begin
+        parity = ^data; // Compute even parity (XOR all bits together)
+    end
+endmodule
